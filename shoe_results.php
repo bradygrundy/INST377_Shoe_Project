bf825ee38b4517b1b04b15df937dd35d8c273ae8
bradygrundy <33431475+bradygrundy@users.noreply.github.com>
2018-11-30 12:45:58 -0500

    Create shoe_results.php

---
+++ shoe_results.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shoe Results</title>
+
+	<style>
+		div {
+			margin-top: 20px;
+			margin-bottom: 20px;
+		}
+	</style>
+</head>
+<body>
+
+<?php
+// The code to process recieved data from the form goes to here.
+$servername = "localhost";
+$username = 'root';
+$password= 'mysql';
+$dbname='';
+?>
+</body>
+</html>
