6eb99d0ad3bd0209c0f8bc6cce6fd0355b2f4280
bradygrundy <33431475+bradygrundy@users.noreply.github.com>
2018-12-03 15:31:33 -0500

    Added php connection

--- shoe_results.php
+++ shoe_results.php
@@ -22,7 +22,10 @@
 	$conn = new mysqli($servername, $username, $password, $dbname);
 
 	//need to add connection variable here
-	$html_id = 5;
+	$html_id= $_POST["total"];
+	echo $html_id;
+	$html_id = ltrim($html_id, "s");
+	echo $html_id;
 
 	$result = mysqli_query($conn, "SELECT * FROM shoe_data WHERE shoe_id='$html_id'");
 	$row = $result->fetch_assoc();
